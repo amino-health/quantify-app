3d74b75a057e9689bbbde25316fb0e098c86d71b
pottfisk <robert.paananen@hotmail.com>
2021-03-29 15:06:35 +0200

    TOS screen example finished

--- quantify_app/lib/screens/main.dart
+++ quantify_app/lib/screens/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quantify_app/screens/homeScreen.dart';
-
-
+import 'package:quantify_app/screens/tos.dart';
 
 
 void main() {
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomeScreen());
+        home: TosScreen());
   }
 }
 
