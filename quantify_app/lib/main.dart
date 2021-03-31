7293baa620d8e9a89be1d2e093d4ecc1f1e7a34c
Erik Junghahn <Erik.Junghahn.8642@student.uu.se>
2021-03-31 14:24:26 +0200

    Created activity screen with view for adding and creating new activities

--- quantify_app/lib/main.dart
+++ quantify_app/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:quantify_app/screens/welcomeScreen.dart';
-//import 'package:quantify_app/screens/addActivityScreen.dart';
+import 'package:quantify_app/screens/addActivityScreen.dart';
+//import 'package:quantify_app/screens/welcomeScreen.dart';
+//import 'package:quantify_app/screens/welcomeScreen.dart';
+//import 'package:quantify_app/screens/createActivityScreen.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
         theme: ThemeData(
-          primaryColor: Color(0xFF99163D),
-        ),
-        home: WelcomeScreen());
+            primaryColor: Color(0xFF99163D), primarySwatch: Colors.red),
+        home: AddActivityScreen());
   }
 }
--- quantify_app/lib/screens/addActivityScreen.dart
+++ quantify_app/lib/screens/addActivityScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quantify_app/screens/homeSkeleton.dart';
+import 'package:quantify_app/screens/ActivityFormScreen.dart';
 //import 'package:flutter_svg/flutter_svg.dart';
 
 class AddActivityScreen extends StatefulWidget {
@@ -11,120 +12,159 @@ class AddActivityScreen extends StatefulWidget {
 
 class _AddActivityScreenState extends State<AddActivityScreen>
     with TickerProviderStateMixin {
+  final _activitySearch = TextEditingController();
+  int _selectedIndex = 0;
+
   TabController _tabController;
 
-  final _activitySearch = TextEditingController();
+  List<Widget> myActivityList = <Widget>[];
+  List<Widget> allActivityList = <Widget>[];
+  List<Widget> historyActivityList = <Widget>[];
+  List<Widget> recentActivityList = <Widget>[];
+
+  final List<Tab> _activityTabs = <Tab>[
+    new Tab(
+      child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text('History', style: TextStyle(color: Colors.black))),
+    ),
+    new Tab(
+      child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text('My Activities', style: TextStyle(color: Colors.black))),
+    ),
+    new Tab(
+      child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text('All Activities', style: TextStyle(color: Colors.black))),
+    ),
+  ];
 
   @override
   void initState() {
     super.initState();
-    _tabController = new TabController(vsync: this, length: 3);
+    _tabController =
+        new TabController(vsync: this, length: _activityTabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   bottomButton(BuildContext context, String _title) {
     return Container(
         child: FittedBox(
             fit: BoxFit.fitWidth,
-            child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.pressed))
-                      return Color(0xDD99163D);
-                    else
-                      return Color(0xFF99163D);
-                  },
-                ),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.zero,
-                      side: BorderSide(color: Color(0xFFF0F0F0), width: 3)),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 25.0, right: 25.0, top: 8, bottom: 8),
-                child: Text(
-                  _title,
-                  style: TextStyle(fontFamily: 'roboto-bold'),
-                ),
-              ),
-              onPressed: () {},
-            )));
+            child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 6.0),
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.pressed))
+                              return Color(0xDD99163D);
+                            else
+                              return Color(0xFF99163D);
+                          },
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        ))),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 25.0, right: 25.0, top: 8, bottom: 8),
+                      child: Text(
+                        _title,
+                        style: TextStyle(fontFamily: 'roboto-bold'),
+                      ),
+                    ),
+                    onPressed: () async {
+                      List<String> activityData = await showDialog(
+                          context: context,
+                          builder: (_) => ActivityPopup(
+                              isAdd: false, titlevalue: '', subtitle: ''));
+                      addItem(context, activityData);
+                    }))));
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      print(index);
+      _selectedIndex = index;
+    });
+  }
+
+  activityItem(BuildContext context, String _name, String _subtitle) {
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.95,
+        height: MediaQuery.of(context).size.height * 0.1,
+        child: Card(
+            child: ListTile(
+                title: Text(_name),
+                subtitle: Text(_subtitle),
+                isThreeLine: false,
+                onTap: () async {
+                  List<String> activityData = await showDialog(
+                      context: context,
+                      builder: (_) => ActivityPopup(
+                          isAdd: true, titlevalue: _name, subtitle: _subtitle));
+                  addActivity(context, activityData);
+                })));
   }
 
   customScrollview(BuildContext context) {
+    List<Widget> activityList = <Widget>[];
+    if (_selectedIndex == 0) {
+      activityList = historyActivityList;
+    }
+    if (_selectedIndex == 1) {
+      activityList = myActivityList;
+    }
+    if (_selectedIndex == 2) {
+      activityList = allActivityList;
+    }
     return Container(
       child: Expanded(
         child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-              activityItem(context, 'test1'),
-            ],
-          ),
+              //mainAxisAlignment: MainAxisAlignment.end,
+              children: activityList),
         ),
       ),
     );
   }
 
-  activityItem(BuildContext context, String _name) {
-    return Container(
-        child: TextButton(
-      child: FittedBox(
-        fit: BoxFit.fitWidth,
-        child: Text(
-          _name,
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-      ),
-      onPressed: () {},
-    ));
+  void addActivity(context, activityData) {
+    setState(() {});
+  }
+
+  void addItem(context, activityData) {
+    setState(() {
+      myActivityList
+          .add(activityItem(context, activityData[0], activityData[1]));
+      historyActivityList
+          .add(activityItem(context, activityData[0], activityData[1]));
+    });
   }
 
   tabBar(BuildContext context) {
-    return TabBar(tabs: <Tab>[
-      new Tab(
-        child: FittedBox(
-            fit: BoxFit.fitWidth,
-            child: Text('History', style: TextStyle(color: Colors.black))),
-      ),
-      new Tab(
-        child: FittedBox(
-            fit: BoxFit.fitWidth,
-            child:
-                Text('My Activities', style: TextStyle(color: Colors.black))),
-      ),
-      new Tab(
-        child: FittedBox(
-            fit: BoxFit.fitWidth,
-            child:
-                Text('All Activities', style: TextStyle(color: Colors.black))),
-      ),
-    ], controller: _tabController);
+    return TabBar(
+      controller: _tabController,
+      tabs: _activityTabs,
+      onTap: _onItemTapped,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: CustomAppBar(),
       body: Center(
           child: Column(
@@ -152,7 +192,7 @@ class _AddActivityScreenState extends State<AddActivityScreen>
               child: tabBar(context),
             ),
           ),
-          customScrollview(context)
+          customScrollview(context),
         ],
       )),
       bottomNavigationBar: bottomButton(context, 'Create New Activity'),
