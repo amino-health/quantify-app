432b17dc388b6071680a96db2ae4217b21315b45
fryn4538 <fryn4538@student.uu.se>
2021-04-06 14:29:20 +0200

    added regex for entering new weight and height and checker for email validation

--- quantify_app/lib/screens/change.dart
+++ quantify_app/lib/screens/change.dart
@@ -1,5 +1,6 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'homeSkeleton.dart';
 
 class Change extends StatefulWidget {
@@ -19,6 +20,8 @@ class _ChangeState extends State<Change> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
     String _unit = widget.toChange == "weight"
         ? " kg"
         : widget.toChange == "height"
@@ -29,75 +32,94 @@ class _ChangeState extends State<Change> {
 
     TextEditingController _inputController = TextEditingController();
 
-    bool _validate = false;
-    String _errorMsg = "";
-
     return Scaffold(
       appBar: CustomAppBar(),
       body: Container(
         padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Current ${widget.toChange}",
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).accentColor,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                _current,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Current ${widget.toChange}",
                 textAlign: TextAlign.left,
-              ),
-            ),
-            Text(
-              "New ${widget.toChange}",
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).accentColor,
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 12),
-              child: TextFormField(
-                controller: _inputController,
-                keyboardType: _change == "email"
-                    ? TextInputType.text
-                    : TextInputType.number,
-                onEditingComplete: () {
-                  String text = _inputController.text;
-                  if (_change == "email") {
-                    EmailValidator.validate(text)
-                        ? _validate = true
-                        : _errorMsg = "Invalid email";
-                  }
-                  if (_change == "height") {
-                    double.parse(text) < 210.0 && double.parse(text) > 100.0
-                        ? _validate = true
-                        : _errorMsg = "Invalid height";
-                  }
-                  if (_change == "weight") {
-                    double.parse(text) < 150.0 && double.parse(text) > 30.0
-                        ? _validate = true
-                        : _errorMsg = "Invalid height";
-                  }
-                },
-                decoration: InputDecoration(
-                  errorText: _validate ? null : _errorMsg,
-                  contentPadding: EdgeInsets.all(4),
-                  isDense: true,
-                  hintText:
-                      '${_change[0].toUpperCase()}${_change.substring(1)}',
-                  border: UnderlineInputBorder(),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).accentColor,
                 ),
               ),
-            ),
-          ],
+              Container(
+                margin: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  _current,
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Text(
+                "New ${widget.toChange}",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).accentColor,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 12, bottom: 10),
+                child: TextFormField(
+                  controller: _inputController,
+                  keyboardType: _change == "email"
+                      ? TextInputType.text
+                      : TextInputType.number,
+                  inputFormatters: _change != "email"
+                      ? <TextInputFormatter>[
+                          FilteringTextInputFormatter.allow(RegExp(
+                              '^\$|^(0|([1-9][0-9]{0,}))(\\.[0-9]{0,})?\$')),
+                        ]
+                      : null,
+                  validator: (text) {
+                    if (_change == "email") {
+                      return EmailValidator.validate(text)
+                          ? null
+                          : "Invalid email";
+                    }
+                    if (_change == "height") {
+                      try {
+                        return double.parse(text) < 252.0 &&
+                                double.parse(text) > 67.0
+                            ? null
+                            : "Invalid height";
+                      } catch (e) {
+                        return "Can only contain digits";
+                      }
+                    }
+                    if (_change == "weight") {
+                      return double.parse(text) < 442.0 &&
+                              double.parse(text) > 30.0
+                          ? null
+                          : "Invalid height";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(4),
+                    isDense: true,
+                    hintText:
+                        '${_change[0].toUpperCase()}${_change.substring(1)}',
+                    border: UnderlineInputBorder(),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text('Save'),
+              ),
+            ],
+          ),
         ),
       ),
     );
